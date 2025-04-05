e9ae0479d211406c580c212061b4fcf8837685e5
a-creamy <akramo@tuta.io>
2025-04-05 08:53:09 +0000

    Input

---
+++ src/calc.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, stdout, Write};
+
+struct Calc {
+    prompt: String,
+}
+
+impl Calc {
+    fn new(prompt: &str) -> Self {
+        Calc {
+            prompt: prompt.to_string(),
+        }
+    }
+
+    fn input(&self) -> String {
+        print!("{} ", self.prompt);
+
+        let mut s = String::new();
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut s)
+            .expect("Did not type in correct string");
+        s.trim().to_string()
+    }
+}
+
+pub fn run() {
+    let calc = Calc::new(">");
+    loop {
+        println!("{}", calc.input());
+    }
+}
