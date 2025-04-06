2e4d153d6f7907f2dd9e9da07609eeccf680acb9
a-creamy <akramo@tuta.io>
2025-04-06 10:57:17 +0000

    Evaluator

--- src/eval.rs
+++ src/eval.rs
@@ -1,10 +1,39 @@
+use types::Token;
+
 mod lexer;
 mod types;
 mod rpn;
 
+fn evaluate_rpn(rpn: Vec<Token>) -> f64 {
+    let mut stack = Vec::new();
+
+    for token in rpn {
+        match token {
+            Token::Number(value) => stack.push(value),
+            Token::Operator(op) => {
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+
+                let result = match op {
+                    '+' => a + b,
+                    '-' => a - b,
+                    '*' => a * b,
+                    '/' => a / b,
+                    '^' => a.powf(b),
+                    _ => panic!("Unknown operator"),
+                };
+
+                stack.push(result);
+            }
+            _ => panic!("Unexpected token in evaluation"),
+        }
+    }
+
+    stack.pop().unwrap()
+}
+
 pub fn run<'a>(input: &str) {
     let tokens = lexer::lex(input);
     let rpn = rpn::into(tokens.clone());
-    println!("Tokens: {:?}", tokens);
-    println!("Rpn: {:?}", rpn);
+    println!("{}", evaluate_rpn(rpn));
 }
