7f12a51322ba31b9783bf439db862bcab7ed61a8
a-creamy <akfain@proton.me>
2025-04-06 15:28:04 +0000

    Replaced main with ast

--- src/eval.rs
+++ src/eval.rs
@@ -1,39 +1,14 @@
-use types::Token;
+use lexer::Lexer;
+use parser::Parser;
 
 mod lexer;
+mod parser;
 mod types;
-mod rpn;
 
-fn evaluate_rpn(rpn: Vec<Token>) -> f64 {
-    let mut stack = Vec::new();
-
-    for token in rpn {
-        match token {
-            Token::Number(value) => stack.push(value),
-            Token::Operator(op) => {
-                let b = stack.pop().unwrap();
-                let a = stack.pop().unwrap();
-
-                let result = match op {
-                    '+' => a + b,
-                    '-' => a - b,
-                    '*' => a * b,
-                    '/' => a / b,
-                    '^' => a.powf(b),
-                    _ => panic!("Unknown operator"),
-                };
-
-                stack.push(result);
-            }
-            _ => panic!("Unexpected token in evaluation"),
-        }
-    }
-
-    stack.pop().unwrap()
-}
-
-pub fn run<'a>(input: &str) {
-    let tokens = lexer::lex(input);
-    let rpn = rpn::into(tokens.clone());
-    println!("{}", evaluate_rpn(rpn));
+pub fn run<'a>(input: &'a str) {
+    let mut lexer = Lexer::new(input);
+    let tokens = lexer.lex();
+    let mut parser = Parser::new(&tokens);
+    println!("{:?}", tokens);
+    println!("{:?}", parser.parse());
 }
---
+++ src/eval/parser.rs
@@ -0,0 +1,95 @@
+use super::types::{Ast, Token};
+use std::{iter::Peekable, slice::Iter};
+
+pub struct Parser<'a> {
+    tokens: Peekable<Iter<'a, Token>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(tokens: &'a [Token]) -> Self {
+        Parser {
+            tokens: tokens.iter().peekable(),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Ast, String> {
+        self.expression(0)
+    }
+
+    fn expression(&mut self, precedence: u8) -> Result<Ast, String> {
+        let mut left = self.primary()?;
+
+        while let Some(&token) = self.tokens.peek() {
+            let token_precedence = Self::get_precedence(token);
+            if token_precedence < precedence {
+                break;
+            }
+
+            let token = self.tokens.next().unwrap();
+            left = self.infix(left, &token)?;
+        }
+
+        Ok(left)
+    }
+
+    fn primary(&mut self) -> Result<Ast, String> {
+        if let Some(token) = self.tokens.next() {
+            match token {
+                Token::Number(value) => Ok(Ast::Number(value.clone())),
+                Token::LeftParen => self.paren(),
+                _ => Err(format!("Unexpected token: {:?}", token)),
+            }
+        } else {
+            Err("Unexpected end of tokens".to_string())
+        }
+    }
+
+    fn paren(&mut self) -> Result<Ast, String> {
+        let mut depth = 1;
+        let mut tokens = Vec::new();
+
+        while let Some(token) = self.tokens.next() {
+            match token {
+                Token::LeftParen => {
+                    depth += 1;
+                    tokens.push(token.clone());
+                }
+                Token::RightParen => {
+                    depth -= 1;
+                    if depth == 0 {
+                        break;
+                    }
+                    tokens.push(token.clone());
+                }
+                _ => tokens.push(token.clone()),
+            }
+        }
+
+        if depth != 0 {
+            return Err("Unclosed parenthesis".into());
+        }
+
+        Parser::new(&tokens).parse()
+    }
+
+    fn infix(&mut self, left: Ast, token: &Token) -> Result<Ast, String> {
+        let precedence = Self::get_precedence(token);
+        let right = self.expression(precedence + 1)?;
+
+        match token {
+            Token::Plus => Ok(Ast::Plus(Box::new(left), Box::new(right))),
+            Token::Minus => Ok(Ast::Minus(Box::new(left), Box::new(right))),
+            Token::Multiply => Ok(Ast::Multiply(Box::new(left), Box::new(right))),
+            Token::Divide => Ok(Ast::Divide(Box::new(left), Box::new(right))),
+            _ => Err(format!("Unexpected infix token: {:?}", token)),
+        }
+    }
+
+    fn get_precedence(token: &Token) -> u8 {
+        match token {
+            Token::Plus | Token::Minus => 1,
+            Token::Multiply | Token::Divide => 2,
+            _ => 0,
+        }
+    }
+}
