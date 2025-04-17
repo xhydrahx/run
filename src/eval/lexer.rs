0a4ecd1d2979d6b3ef72ca024f59808968e7de52
a-creamy <akramo@tuta.io>
2025-04-17 13:34:12 +0200

    Optimize the lexer

--- src/eval/lexer.rs
+++ src/eval/lexer.rs
@@ -22,7 +22,7 @@ impl<'a> Lexer<'a> {
 
     pub fn lex(&mut self) -> Result<Vec<Token>, String> {
         let mut tokens = Vec::new();
-        while let Some(mut c) = self.current_char {
+        while let Some(c) = self.current_char {
             match c {
                 ' ' | '\n' | '\t' => self.advance(),
                 c if c.is_digit(10) || c == '.' => {
@@ -62,8 +62,10 @@ impl<'a> Lexer<'a> {
                     self.advance();
                 }
                 'r' => {
-                    if self.check_for("root") {
+                    if self.check_for(&["root"]).is_some() {
                         tokens.push(Token::Root);
+                    } else {
+                        return Err("Unknown identifier starting with 'r'".into());
                     }
                 }
                 'e' => {
@@ -71,76 +73,52 @@ impl<'a> Lexer<'a> {
                     self.advance();
                 }
                 'p' => {
-                    self.advance();
-                    if self.current_char == Some('i') {
-                        tokens.push(Token::Pi);
-                        self.advance();
-                    } else if self.current_char == Some('h') {
-                        self.advance();
-                        if self.current_char == Some('i') {
-                            tokens.push(Token::Phi);
-                            self.advance();
+                    if let Some(keyword) = self.check_for(&["pi", "phi"]) {
+                        match keyword.as_str() {
+                            "pi" => tokens.push(Token::Pi),
+                            "phi" => tokens.push(Token::Phi),
+                            _ => unreachable!(),
                         }
+                    } else {
+                        return Err("Unknown identifier starting with 'p'".into());
                     }
                 }
                 'l' => {
-                    let mut ident = String::new();
-                    for _i in 0..3 {
-                        if self.current_char.is_none() {
-                            break;
+                    if let Some(keyword) = self.check_for(&["ln", "log"]) {
+                        match keyword.as_str() {
+                            "ln" => {
+                                tokens.push(Token::Ln);
+                                if self.current_char == Some('(') {
+                                    tokens.push(Token::LeftParen);
+                                    self.advance();
+                                }
+                            }
+                            "log" => tokens.push(Token::Log),
+                            _ => unreachable!(),
                         }
-                        c = self.current_char.unwrap();
-                        if c.is_alphabetic() {
-                            ident.push(c);
-                            self.advance();
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if ident == "log" {
-                        tokens.push(Token::Log);
-                    } else if ident == "ln" {
-                        tokens.push(Token::Ln);
-                        if self.current_char == Some('(') {
-                            tokens.push(Token::LeftParen);
-                            self.advance();
-                        }
+                    } else {
+                        return Err("Unknown identifier starting with 'l'".into());
                     }
                 }
                 's' => {
-                    let mut ident = String::new();
-                    for _i in 0..4 {
-                        if self.current_char.is_none() {
-                            break;
-                        }
-                        c = self.current_char.unwrap();
-                        if c.is_alphabetic() {
-                            ident.push(c);
-                            self.advance();
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if ident == "sqrt" {
-                        tokens.push(Token::Sqrt);
-                    } else if ident == "sin" {
+                    if self.check_for(&["sin"]).is_some() {
                         tokens.push(Token::Sin);
-                        if self.current_char == Some('(') {
-                            tokens.push(Token::LeftParen);
-                            self.advance();
-                        }
+                    } else {
+                        return Err("Unknown identifier starting with 's'".into());
                     }
                 }
                 'c' => {
-                    if self.check_for("cos") {
+                    if self.check_for(&["cos"]).is_some() {
                         tokens.push(Token::Cos);
+                    } else {
+                        return Err("Unknown identifier starting with 'c'".into());
                     }
                 }
                 't' => {
-                    if self.check_for("tan") {
+                    if self.check_for(&["tan"]).is_some() {
                         tokens.push(Token::Tan);
+                    } else {
+                        return Err("Unknown identifier starting with 't'".into());
                     }
                 }
                 '!' => {
@@ -171,14 +149,12 @@ impl<'a> Lexer<'a> {
         Ok(tokens)
     }
 
-    fn check_for(&mut self, name: &str) -> bool {
-        let mut c;
+    fn check_for(&mut self, keywords: &[&str]) -> Option<String> {
+        let saved_input = self.input.clone();
+        let saved_char = self.current_char;
+
         let mut ident = String::new();
-        for _i in 0..name.len() {
-            if self.current_char.is_none() {
-                break;
-            }
-            c = self.current_char.unwrap();
+        while let Some(c) = self.current_char {
             if c.is_alphabetic() {
                 ident.push(c);
                 self.advance();
@@ -187,6 +163,14 @@ impl<'a> Lexer<'a> {
             }
         }
 
-        return ident == name;
+        for &keyword in keywords {
+            if ident == keyword {
+                return Some(ident);
+            }
+        }
+
+        self.input = saved_input;
+        self.current_char = saved_char;
+        None
     }
 }
