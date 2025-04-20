61c35c8f914daf4648e65d33e07463388e927255
a-creamy <akfain@proton.me>
2025-04-20 14:26:07 +0200

    Evaluate expressions & cleanup

--- src/eval.rs
+++ src/eval.rs
@@ -1,17 +1,41 @@
 mod lexer;
-mod types;
 mod parser;
+mod types;
 use super::eval::lexer::Lexer;
 use super::eval::parser::Parser;
+use super::eval::types::{Expr, Operator};
 
 pub fn eval(input: &str) {
     let mut lexer = Lexer::new(input);
-    let tokens = lexer.lex();
-    println!("=> {:?}", tokens);
-    let binding = tokens.unwrap();
-    let mut parser = Parser::new(&binding);
+    let tokens = match lexer.lex() {
+        Ok(value) => value,
+        Err(e) => {
+            eprintln!("=> {}", e);
+            return;
+        }
+    };
+
+    let mut parser = Parser::new(&tokens);
     match parser.parse() {
-        Ok(ast) => println!("=> {:?}", ast),
+        Ok(ast) => println!("=> {}", calculate(&ast)),
         Err(e) => eprintln!("=> {}", e),
+    }
+}
+
+fn calculate(expr: &Expr) -> f64 {
+    match expr {
+        Expr::Num(n) => *n,
+        Expr::BinaryOp(left, op, right) => {
+            let l = calculate(left);
+            let r = calculate(right);
+
+            match op {
+                Operator::Addition => l + r,
+                Operator::Subtraction => l - r,
+                Operator::Multiplication => l * r,
+                Operator::Division => l / r,
+                Operator::Exponent => l.powf(r),
+            }
+        }
     }
 }
