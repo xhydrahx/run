d3ca8625b88df31171d1a21d9945d2802a1f14df
Hydra <akramo@tuta.io>
2025-07-21 22:21:47 +0200

    No need for a object for lexer

--- src/eval.rs
+++ src/eval.rs
@@ -4,8 +4,8 @@ pub mod lexer;
 pub mod parser;
 pub mod types;
 
-pub fn evaluate(input: &str) -> Result<f64, String> {
+pub fn evaluate(expr: &str) -> Result<f64, String> {
     Ok(executor::calculate(
-        parser::Parser::new(&lexer::Lexer::new(input).lex()?).parse()?,
+        parser::Parser::new(&lexer::lex(&mut expr.chars().peekable())?).parse()?,
     ))
 }
--- src/eval/lexer.rs
+++ src/eval/lexer.rs
@@ -1,113 +1,101 @@
 use crate::eval::types::Token;
 use std::{iter::Peekable, str::Chars};
 
-pub struct Lexer<'a> {
-    expr: Peekable<Chars<'a>>,
-}
+pub fn lex(expr: &mut Peekable<Chars>) -> Result<Vec<Token>, String> {
+    let mut tokens = Vec::new();
 
-impl<'a> Lexer<'a> {
-    pub fn new(input: &'a str) -> Self {
-        Lexer {
-            expr: input.chars().peekable(),
-        }
-    }
-
-    pub fn lex(&mut self) -> Result<Vec<Token>, String> {
-        let mut tokens = Vec::new();
-
-        while let Some(c) = self.expr.peek() {
-            match c {
-                '0'..='9' | '.' => {
-                    let mut num = String::new();
-                    while let Some(c) = self.expr.peek() {
-                        if c.is_digit(10) || c == &'.' {
-                            num.push(*c);
-                        } else {
-                            break;
-                        }
-                        self.expr.next();
+    while let Some(c) = expr.peek() {
+        match c {
+            '0'..='9' | '.' => {
+                let mut num = String::new();
+                while let Some(c) = expr.peek() {
+                    if c.is_digit(10) || c == &'.' {
+                        num.push(*c);
+                    } else {
+                        break;
                     }
-                    tokens.push(Token::Num(
-                        num.parse::<f64>()
-                            .expect("Failed to parse a string into a number"),
-                    ));
+                    expr.next();
                 }
-                'a'..='z' | 'A'..='Z' => {
-                    let mut identifier = String::new();
-                    while let Some(c) = self.expr.peek() {
-                        if c.is_alphabetic() {
-                            identifier.push(*c);
-                        } else {
-                            break;
-                        }
-                        self.expr.next();
+                tokens.push(Token::Num(
+                    num.parse::<f64>()
+                        .expect("Failed to parse a string into a number"),
+                ));
+            }
+            'a'..='z' | 'A'..='Z' => {
+                let mut identifier = String::new();
+                while let Some(c) = expr.peek() {
+                    if c.is_alphabetic() {
+                        identifier.push(*c);
+                    } else {
+                        break;
                     }
+                    expr.next();
+                }
 
-                    tokens.push(Token::Identifier(identifier));
-                }
-                '+' => {
-                    tokens.push(Token::Plus);
-                    self.expr.next();
-                }
-                '-' => {
-                    tokens.push(Token::Minus);
-                    self.expr.next();
-                }
-                '*' => {
-                    tokens.push(Token::Star);
-                    self.expr.next();
-                }
-                '/' => {
-                    tokens.push(Token::Slash);
-                    self.expr.next();
-                }
-                '^' => {
-                    tokens.push(Token::Carrot);
-                    self.expr.next();
-                }
-                '(' => {
-                    tokens.push(Token::LeftParen);
-                    self.expr.next();
-                }
-                ')' => {
-                    tokens.push(Token::RightParen);
-                    self.expr.next();
-                }
-                '!' => {
-                    tokens.push(Token::Exclamation);
-                    self.expr.next();
-                }
-                ',' => {
-                    tokens.push(Token::Comma);
-                    self.expr.next();
-                }
-                '_' => {
-                    tokens.push(Token::Underscore);
-                    self.expr.next();
-                }
-                '%' => {
-                    tokens.push(Token::Percent);
-                    self.expr.next();
-                }
-                '|' => {
-                    tokens.push(Token::Bar);
-                    self.expr.next();
-                }
-                '=' => {
-                    tokens.push(Token::Equal);
-                    self.expr.next();
-                }
-                ' ' | '\t' | '\n' => {
-                    self.expr.next();
-                }
-                _ => {
-                    return Err(format!(
-                        "Unknown token '{}': This token is not recognized as part of a valid expression. Check for typos or invalid characters.",
-                        c
-                    ));
-                }
+                tokens.push(Token::Identifier(identifier));
+            }
+            '+' => {
+                tokens.push(Token::Plus);
+                expr.next();
+            }
+            '-' => {
+                tokens.push(Token::Minus);
+                expr.next();
+            }
+            '*' => {
+                tokens.push(Token::Star);
+                expr.next();
+            }
+            '/' => {
+                tokens.push(Token::Slash);
+                expr.next();
+            }
+            '^' => {
+                tokens.push(Token::Carrot);
+                expr.next();
+            }
+            '(' => {
+                tokens.push(Token::LeftParen);
+                expr.next();
+            }
+            ')' => {
+                tokens.push(Token::RightParen);
+                expr.next();
+            }
+            '!' => {
+                tokens.push(Token::Exclamation);
+                expr.next();
+            }
+            ',' => {
+                tokens.push(Token::Comma);
+                expr.next();
+            }
+            '_' => {
+                tokens.push(Token::Underscore);
+                expr.next();
+            }
+            '%' => {
+                tokens.push(Token::Percent);
+                expr.next();
+            }
+            '|' => {
+                tokens.push(Token::Bar);
+                expr.next();
+            }
+            '=' => {
+                tokens.push(Token::Equal);
+                expr.next();
+            }
+            ' ' | '\t' | '\n' => {
+                expr.next();
+            }
+            _ => {
+                return Err(format!(
+                    "Unknown token '{}': This token is not recognized as part of a valid expression. Check for typos or invalid characters.",
+                    c
+                ));
             }
         }
-        Ok(tokens)
     }
+    Ok(tokens)
 }
