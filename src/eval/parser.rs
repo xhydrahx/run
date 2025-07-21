14bd7d3047c20d15e6881d90e7dd2b622f96eee2
Hydra <akramo@tuta.io>
2025-07-21 22:16:03 +0200

    Much cleaner

--- src/eval/parser.rs
+++ src/eval/parser.rs
@@ -1,5 +1,7 @@
-use super::types::{Expr, Operator, Token};
-use crate::eval::identifier;
+use crate::eval::{
+    identifier,
+    types::{Expr, Operator, Token},
+};
 use std::{iter::Peekable, slice::Iter};
 
 pub struct Parser<'a> {
