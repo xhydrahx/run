5b5d06bfd2ce8b4ac298b868b0f4fdf144502bf7
Hydra <akramo@tuta.io>
2025-07-22 15:02:15 +0200

    Variable.rs

--- src/eval/parser/identifier.rs
+++ src/eval/parser/identifier.rs
@@ -1,9 +1,8 @@
 use std::{iter::Peekable, slice::Iter};
 
 use crate::eval::{
-    parser::{self, function},
+    parser::{function, variable},
     types::{Expr, Token},
-    environment,
 };
 
 pub fn parse(tokens: &mut Peekable<Iter<Token>>, id: &str) -> Result<Expr, String> {
@@ -14,30 +13,6 @@ pub fn parse(tokens: &mut Peekable<Iter<Token>>, id: &str) -> Result<Expr, Strin
             function::parse(tokens, id)
         }
 
-        _ => {
-            {
-                let variables = environment::fetch_variables().lock().unwrap();
-                for expr in variables.iter() {
-                    if let Expr::Variable(ident, value) = expr {
-                        if ident.as_str() == id {
-                            return Ok(Expr::Variable(ident.to_string(), value.to_owned()));
-                        }
-                    }
-                }
-
-                if tokens.peek() != Some(&&Token::Equal) {
-                    return Err(format!(
-                        "Unknown variable '{}': Expected a valid variable that has been defined",
-                        id
-                    ));
-                }
-            }
-
-            tokens.next();
-            let expr = parser::primary(tokens, 0)?;
-            let mut variables = environment::fetch_variables().lock().unwrap();
-            variables.push(Expr::Variable(id.to_string(), Box::new(expr)));
-            return Ok(Expr::Num(1.0));
-        }
+        _ => variable::parse(tokens, id),
     }
 }
