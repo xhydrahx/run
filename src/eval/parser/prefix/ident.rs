aece2d2059864756a595693ad360fad6b872197d
Hydra <akramo@tuta.io>
2025-07-23 16:57:55 +0200

    Shorten names

--- src/eval/parser/prefix/ident.rs
+++ src/eval/parser/prefix/ident.rs
@@ -4,17 +4,17 @@ use crate::eval::{
     types::{Expr, Token},
 };
 
-pub mod function;
-pub mod variable;
+pub mod func;
+pub mod var;
 
 pub fn parse(tokens: &mut Peekable<Iter<Token>>, id: &str) -> Result<Expr, String> {
     match id {
         "sqrt" | "ln" | "root" | "log" | "cbrt" | "sin" | "cos" | "tan" | "cot" | "sec" | "csc"
         | "asin" | "acos" | "atan" | "acot" | "asec" | "acsc" | "sinh" | "cosh" | "tanh"
         | "coth" | "sech" | "csch" | "asinh" | "acosh" | "atanh" | "acoth" | "asech" | "acsch" => {
-            function::parse(tokens, id)
+            func::parse(tokens, id)
         }
 
-        _ => variable::parse(tokens, id),
+        _ => var::parse(tokens, id),
     }
 }
