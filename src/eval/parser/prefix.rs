c3946c4dba31a31f76aaa2f17ec94cd7ce367093
Hydra <akramo@tuta.io>
2025-07-23 16:57:55 +0200

    Shorten names

--- src/eval/parser/prefix.rs
+++ src/eval/parser/prefix.rs
@@ -5,7 +5,7 @@ use crate::eval::{
     types::{Expr, Operator, Token},
 };
 
-pub mod identifier;
+pub mod ident;
 
 pub fn parse(tokens: &mut Peekable<Iter<Token>>) -> Result<Expr, String> {
     match tokens.next() {
@@ -14,12 +14,12 @@ pub fn parse(tokens: &mut Peekable<Iter<Token>>) -> Result<Expr, String> {
             Some(Token::Minus) => match tokens.next() {
                 Some(Token::Num(n)) => Ok(Expr::Unary(Operator::Subtraction, Box::new(number::parse(tokens, *n)?))),
                 Some(Token::LeftParen) => Ok(Expr::Unary(Operator::Subtraction, Box::new(delimeter::paren(tokens)?))),
-                Some(Token::Identifier(id)) => Ok(Expr::Unary(Operator::Subtraction, Box::new(identifier::parse(tokens, id)?))),
+                Some(Token::Identifier(id)) => Ok(Expr::Unary(Operator::Subtraction, Box::new(ident::parse(tokens, id)?))),
                 Some(token) => Err(format!("Unexpected token '{}' after unary '-': Expected a number, an opening parenthesis '(', or a valid unary expression.", token)),
                 None => Err("Unexpected end of expression: Expected a number, '(', or unary operator before end.".into()),
             },
-            Some(Token::Identifier(id)) => identifier::parse(tokens, id),
-            Some(Token::Bar) => identifier::function::absolute(tokens),
+            Some(Token::Identifier(id)) => ident::parse(tokens, id),
+            Some(Token::Bar) => ident::func::absolute(tokens),
             Some(token) => Err(format!(
                 "Unexpected token '{}' encountered: Expected a number, an opening parenthesis '(', or a unary operator.",
                 token
